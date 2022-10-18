1dc27261889c83478664fc174a287f5e4d51093e
Jazhiel Laruan <92614307+jajagel@users.noreply.github.com>
2022-10-18 19:40:18 +0800

    Update student.php

--- student.php
+++ student.php
@@ -10,10 +10,10 @@
 	<div class="mx-auto p-5" style="background-color: #FFFFFF; max-width: 60%;" >
 			<form action = "studinfo.php" method = "get">
 				<h5 class="text-center mb-3">Student Information</h5>
-				First Name: <input class="form-control" size = 10px; type = "text" name = "fname"><br>
-				Last Name: <input class="form-control" type = "text" name = "lname"><br>
-				Middle Initial: <input class="form-control" type = "text" name = "mname" maxlength = "1"><br>
-				School ID: <input class="form-control" type = "text" name = "skulid" maxlength = "11"><br>
+				First Name: <input class="form-control" size = 10px; type = "text" name = "fname"><br/>
+				Last Name: <input class="form-control" type = "text" name = "lname"><br/>
+				Middle Initial: <input class="form-control" type = "text" name = "mname" maxlength = "1"><br/>
+				School ID: <input class="form-control" type = "text" name = "skulid" maxlength = "11"><br/>
 				Course: <select class="form-control dropdown-toggle" data-toggle="dropdown" name ="course">
 							<option selected="selected">BSIT</option>
 							<option>BSED</option>
@@ -21,17 +21,17 @@
 							<option>BSF</option>
 							<option>BSCRIM</option>
 							<option>BSCS</option>
-						</select><br>
-				Address: <input class="form-control" type = "text" name = "address"><br>
-				Birthdate: <input class="form-control"  type = "date" name = "bday"><br>
+						</select><br/>
+				Address: <input class="form-control" type = "text" name = "address"><br/>
+				Birthdate: <input class="form-control"  type = "date" name = "bday"><br/>
 
 				<h5 class="text-center mt-4 mb-3">Contact Person or Guardian Information</h5>
-				Contact Person Name: <input class="form-control" type = "text" name = "cpname"><br>
+				Contact Person Name: <input class="form-control" type = "text" name = "cpname"><br/>
 				Contact Person Number: <input class="form-control" name="cpnum"
 									    oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
 									    type = "number"
 									    maxlength = "11"
-									 /><br>
+									 /><br/>
 	
 				<input class ="btn btn-primary btn-block mt-2 mb-5" style="background-color: #053012; width:100%;"type ="submit" name="submit">
 			</form>
